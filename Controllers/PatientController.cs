fd08e23e31e8649248179d31fe36947cd4eda47e
Ramir Junior <junior.ribeiro0@live.com>
2023-12-13 10:50:26 -0300

    Create Patient Controller close #1

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace consulta_viva_API2.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase {
+    }
+}
